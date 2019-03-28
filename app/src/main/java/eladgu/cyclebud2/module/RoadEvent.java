285e2befe848d228006544a79d99eac05e67a766
SOFIA1234567890987 <sofiadimant@gmail.com>
2019-03-28 21:14:52 +0200

    add utils

---
+++ app/src/main/java/eladgu/cyclebud2/module/RoadEvent.java
@@ -0,0 +1,53 @@
+package eladgu.cyclebud2.module;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class RoadEvent {
+
+    @SerializedName("lat")
+    @Expose
+    private Double lat;
+    @SerializedName("lng")
+    @Expose
+    private Double lng;
+    @SerializedName("type")
+    @Expose
+    private Integer type;
+    @SerializedName("description")
+    @Expose
+    private String description;
+
+    public Double getLat() {
+        return lat;
+    }
+
+    public void setLat(Double lat) {
+        this.lat = lat;
+    }
+
+    public Double getLng() {
+        return lng;
+    }
+
+    public void setLng(Double lng) {
+        this.lng = lng;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
+
