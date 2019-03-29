068a978d2e93129b9820d9c1d5a404b9b0442c13
SOFIA1234567890987 <sofiadimant@gmail.com>
2019-03-29 05:45:55 +0300

    initial commit

---
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.test;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(new ViewPagerAdapter(getSupportFragmentManager()));
+
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
---
+++ app/src/main/java/com/example/test/ViewPagerAdapter.java
@@ -0,0 +1,37 @@
+package com.example.test;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.example.test.ChildFragment1;
+import com.example.test.ChildFragment2;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    private Fragment[] childFragments;
+
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+        childFragments = new Fragment[] {
+                new ChildFragment1(), //0
+                new ChildFragment2(), //1
+        };
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return childFragments[position];
+    }
+
+    @Override
+    public int getCount() {
+        return childFragments.length; //3 items
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        String title = getItem(position).getClass().getName();
+        return title.subSequence(title.lastIndexOf(".") + 1, title.length());
+    }
+}
